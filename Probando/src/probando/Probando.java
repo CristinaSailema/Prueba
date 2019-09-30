1b17b4ed280a20b4e57128ec22bb49d806bedfd6
Patricia-Asaz <44324525+Patricia-Asaz@users.noreply.github.com>
2019-09-30 10:06:27 -0500

    Update Probando.java

--- Probando/src/probando/Probando.java
+++ Probando/src/probando/Probando.java
@@ -17,6 +17,7 @@ public class Probando {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Niños");
+        System.out.println("Patty");
     }
     
 }
